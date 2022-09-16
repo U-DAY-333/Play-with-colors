51003355e8201ea6e46fcdde6be861fb87eaf485
U-DAY-333 <saiudaykiranshinagam@gmail.com>
2022-09-16 11:59:49 +0530

    Play with colors

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("We are glad to welcome you.\r\nHope you will enjoy");
+
+var color  = prompt("Enter the background color you want to change");
+document.body.style.background = color;
+//
+// document.querySelector("h1").classList.add("huge");
